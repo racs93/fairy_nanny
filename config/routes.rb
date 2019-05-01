34a3cb08a25857bd62a7b9dffa094647ac0d3149
racs93 <rodrigocorrea2@hotmail.com>
2019-05-01 14:01:36 -0500

    Adding the content to static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,9 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
-
   get 'static_pages/help'
-
   get 'static_pages/historia'
-
   get 'static_pages/nannies'
-
   get 'static_pages/clientes'
-
   get 'static_pages/servicios'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'application#hello'
 end
