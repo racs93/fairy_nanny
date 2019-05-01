aeb0fc6823bfbedea0edf364927218fcb38be36d
racs93 <rodrigocorrea2@hotmail.com>
2019-05-01 12:17:38 -0500

    Add a static Pages controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,19 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def help
+  end
+
+  def historia
+  end
+
+  def nannies
+  end
+
+  def clientes
+  end
+
+  def servicios
+  end
+end
---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+  end
+
+  test "should get help" do
+    get static_pages_help_url
+    assert_response :success
+  end
+
+  test "should get historia" do
+    get static_pages_historia_url
+    assert_response :success
+  end
+
+  test "should get nannies" do
+    get static_pages_nannies_url
+    assert_response :success
+  end
+
+  test "should get clientes" do
+    get static_pages_clientes_url
+    assert_response :success
+  end
+
+  test "should get servicios" do
+    get static_pages_servicios_url
+    assert_response :success
+  end
+
+end
