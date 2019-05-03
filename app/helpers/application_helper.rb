9b931a035b6e2452b3eb27603fcfa07f8c07b83b
racs93 <rodrigocorrea2@hotmail.com>
2019-05-03 02:05:07 -0500

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = "Ellos se divierten, Ustedes tambien"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
