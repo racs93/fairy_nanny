f78ca422df69fcce6a1e55bd20dbdec61b4f1542
Hasan Diwan <hd1@jsc.d8u.us>
2019-05-21 22:11:40 -0700

    test for validity, not for exception

--- test/models/job_test.rb
+++ test/models/job_test.rb
@@ -2,9 +2,8 @@ require 'test_helper'
 
 class JobTest < ActiveSupport::TestCase
   test 'invalid hours' do
-    assert_raise(ActiveModel::ValidationError) do
-      j = Job.new(:hours => 0, :kids => 1, :place => 1, :is_language_necessary => false, :is_toy_necessary => false, :is_spact => false).save!
-    end
+    j = Job.new(:hours => 0, :kids => 1, :place => 1, :is_language_necessary => false, :is_toy_necessary => false, :is_spact => false)
+    refute j.valid?
   end
 
   # test "the truth" do
