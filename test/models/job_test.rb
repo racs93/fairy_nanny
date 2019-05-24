5e82527db0e1af4f23a42adc9bcb71cb215fa54b
Hasan Diwan <hd1@jsc.d8u.us>
2019-05-24 13:43:24 -0700

    fix merge

--- test/models/job_test.rb
+++ test/models/job_test.rb
@@ -1,15 +1,9 @@
 require 'test_helper'
 
 class JobTest < ActiveSupport::TestCase
-<<<<<<< HEAD
-=======
   test 'invalid hours' do
     j = Job.new(:hours => 0, :kids => 1, :place => 1, :is_language_necessary => false, :is_toy_necessary => false, :is_spact => false)
     refute j.valid?
   end
 
->>>>>>> f78ca422df69fcce6a1e55bd20dbdec61b4f1542
-  # test "the truth" do
-  #   assert true
-  # end
 end
