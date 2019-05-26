e874e272b440c72215eaf721b3ee89804150f3e5
Rodrigo <rodrigocorrea2@hotmail.com>
2019-05-26 18:07:40 -0500

    Adding test to footer for addres

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,8 +3,16 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   test "should not be found" do
-      assert_raises(ActionController::RoutingError) do 
+      assert_raises(ActionController::RoutingError) do
         get '/hasan'
       end
+  end
+
+  test "should have a button for the addres on the footer" do
+    page.should have_css('.icon icon-map=marker')
+  end
+
+  test "should give the addres on to footer" do
+    page.should have_text /Magnolias Chuburna, Merida, Yucatán, México/
   end
 end
