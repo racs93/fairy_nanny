a648f15e134747d0500eb42e3c3dd76084f7a1eb
Rodrigo <rodrigocorrea2@hotmail.com>
2019-06-03 21:40:38 -0500

    adding test to historia section

--- test/system/home_page_contents_test.rb
+++ test/system/home_page_contents_test.rb
@@ -6,6 +6,8 @@ class HomePageContentsTest < ApplicationSystemTestCase
   #
   #   assert_selector "h1", text: "HomePageContent"
   # end
+
+  #home secction
   test "should show the email address somewhere on the page" do
       visit '/'
       assert page.has_content? 'social@fairynanny.mx'
@@ -18,6 +20,7 @@ class HomePageContentsTest < ApplicationSystemTestCase
 
   test "home should have saber mas button" do
     visit '/'
+    page.execute_script "window.scrollBy(0,600)"
     assert click_link('Saber Más')
   end
 
@@ -31,5 +34,31 @@ class HomePageContentsTest < ApplicationSystemTestCase
     assert page.must_have_content "Profesionales en el cuidado y entretenimiento infantil"
     assert page.must_have_content "Fairy Nanny"
     assert page.must_have_content "Fairy World"
+  end
+
+  #historia secction
+  test "historia should say historia" do
+    visit '/historia'
+    assert page.must_have_content "Historia"
+  end
+
+  test "historia should have Nuestras nannies section" do
+    visit '/historia'
+    assert page.must_have_content "Nuestras Nannies"
+    assert page.must_have_content "Proceso de Reclutamiento"
+    assert page.must_have_content "Cursos Mensuales"
+    assert page.must_have_content "Enfoques de los cursos"
+    assert page.must_have_content "Clientes Seguros"
+    assert page.must_have_content "Club de niños"
+    assert page.must_have_content "Talleres para fiestas"
+    assert page.must_have_content "Cursos privados"
+  end
+
+  test "historia should have a banner with the experience and other data" do
+    visit '/historia'
+    page.execute_script "window.scrollBy(0,10000)"
+    assert page.must_have_content "Profesionales en el cuidado y entretenimiento infantil"
+    assert page.must_have_content "11"
+    assert page.must_have_content  "Años"
   end
 end
